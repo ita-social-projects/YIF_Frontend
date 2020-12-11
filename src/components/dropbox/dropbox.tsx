b37bcc999ab1fdbe11ad9e49d8122e294f16fa86
Roman Serhiychuk <Rominos7@gmail.com>
2020-12-11 15:35:37 +0200

    final version without fonts

--- src/components/dropbox/dropbox.tsx
+++ src/components/dropbox/dropbox.tsx
@@ -7,6 +7,11 @@ import {
 import DropboxElement from '../common/dropbox/dropbox'
 import {ReactComponent as SearchIcon} from './search.svg'
 
+import {ReactComponent as Filter1} from './filter1.svg'
+import {ReactComponent as Filter2} from './filter2.svg'
+import {ReactComponent as Filter3} from './filter3.svg'
+
+
 const Dropbox =()=>{
     
     const state = useSelector(selectData); 
@@ -26,17 +31,17 @@ const Dropbox =()=>{
                 <div className="selectors">
                   <DropboxElement data={direction} keyId={0} listName={'Direction'} ></DropboxElement>               
                   <DropboxElement data={speciality} keyId={1} listName={'Speciality'} width={348}></DropboxElement>
-                  <DropboxElement data={university} keyId={2} listName={'University'}  ></DropboxElement>       
+                  <DropboxElement data={university} keyId={2} listName={'University'} ></DropboxElement>       
                 </div>      
             </form>
             <button type={"submit"} form='univ_filter'>
                   <span className='searchText'>Search</span>
                   <span className='searchIcon'><SearchIcon></SearchIcon></span>
             </button>
-            
-            </div>           
-            
-             
+            </div>
+            <div className="filter1"><Filter1></Filter1></div>
+            <div className="filter2"><Filter2></Filter2></div>
+            <div className="filter3"><Filter3></Filter3></div>                
         </div>
       </Fragment>
     )
