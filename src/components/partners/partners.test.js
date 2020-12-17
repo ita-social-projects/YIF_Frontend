9d454c97f5a5bb19dbe4d6dcb988d0fc4a94cdf3
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-12-17 16:19:16 +0200

    the test was added

--- src/components/partners/partners.test.js
+++ src/components/partners/partners.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Partners} from "../index";
+import {PartnerComponent} from './partnersComponent/partnerComponent'
 import { render, fireEvent, screen} from "@testing-library/react";
 
 
@@ -21,4 +22,14 @@ test('check if user can scroll the slide', () => {
   fireEvent.click(buttonRight);
   fireEvent.click(buttonLeft);
   expect(onClick).toHaveBeenCalledTimes(1);
+});
+
+test('check fragment', () =>{
+  const { getByText, asFragment } = render(<Partners />)
+  const firstRender = asFragment();
+});
+
+test('check rerender', () =>{
+  const { rerender } = render(<Partners number={1} />)
+  rerender(<Partners number={2} />)
 });
