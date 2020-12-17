3427c986e0e7c390278dbdde677c9ffc40a4b8f7
Larysa <larisa.shashuk@gmail.com>
2020-12-17 18:34:06 +0200

    added about us component with styles

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ export { default as App } from './app';
 export { default as Counter } from './counter';
 export { default as Header } from './header';
 export { default as Banner } from './banner';
+export { default as AboutUs } from './aboutUs';
 export { default as BannerLower } from './bannerLower';
 export { default as Partners } from './partners';
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './home.module.scss';
-import { Header, Banner, BannerLower, Partners } from '../../components';
+import {
+  Header,
+  Banner,
+  BannerLower,
+  Partners,
+  AboutUs,
+} from '../../components';
 import ErrorBoundry from '../../errorBoundry';
 
 import FakeRequest from '../../components/_fakeRequest'; // delete later
@@ -12,7 +18,7 @@ const Home = () => {
         <Header />
         <Banner />
         <div className={styles.filter}>Filter</div>
-        <div className={styles.about}>About</div>
+        <AboutUs />
         <BannerLower />
         <Partners />
         <div className={styles.footer}>Footer</div>
