21a2138a3228d0387eb380f2b181cf08a9a7a791
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-12-30 11:53:35 +0200

    test was created

--- src/components/registrationForm/registrationForm.test.js
+++ src/components/registrationForm/registrationForm.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import RegistrationForm from './registrationForm'
-import { MemoryRouter } from 'react-router-dom';
-import {fireEvent, render, screen} from "@testing-library/react";
+import {MemoryRouter} from 'react-router-dom';
+import { render, screen} from "@testing-library/react";
+import RegistrationPage from "./index";
+import {unmountComponentAtNode} from "react-dom";
 
-describe('registrationForm',() => {
+
+describe('registrationForm', () => {
 
   test('render a title', () => {
     const {getByText} = render(
         <MemoryRouter>
-          <RegistrationForm />
+          <RegistrationForm/>
         </MemoryRouter>
     );
     const text = screen.getByText(/зареєстровані/i)
@@ -17,17 +20,64 @@ describe('registrationForm',() => {
 
   });
 
-  /*test('check the button', () => {
+  test('checking placeholderText', () => {
     const {getByText} = render(
         <MemoryRouter>
-          <RegistrationForm />
+          < RegistrationForm/>
         </MemoryRouter>
     );
-    const onClick = jest.fn();
-    const button = screen.getByTestId('button');
-    render(< RegistrationForm onClick={onClick()} />);
-    fireEvent.click(button);
-    expect(onClick).toHaveBeenCalledTimes(1);
-  });*/
+    const inputEmail = screen.getByPlaceholderText('Електронна пошта');
+    const inputPassword = screen.getByPlaceholderText('Пароль');
+    const inputConfirmPassword = screen.getByPlaceholderText('Підтвердіть пароль');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    expect(inputConfirmPassword).toBeInTheDocument();
+
+  });
+
+  test('checking to render img', () => {
+    const {getByText} = render(
+        <MemoryRouter>
+          < RegistrationForm/>
+        </MemoryRouter>
+    );
+    const img = screen.getByAltText('win');
+    expect(img).toBeInTheDocument();
+
+  });
+
+  test('checking to render wrapper', () => {
+    const {getByText} = render(
+        <MemoryRouter>
+          < RegistrationForm/>
+        </MemoryRouter>
+    );
+    const section = screen.getByRole('section');
+    expect(section).toBeInTheDocument();
+    expect(section.tagName).toMatch(/SECTION/i);
+    const wrapper = screen.getByRole('wrapper');
+    expect(wrapper).toBeInTheDocument();
+    expect(wrapper.tagName).toMatch(/DIV/i);
+  });
+
+});
+
+describe ('registrationPage', () => {
+
+  test('checking to render wrapper', () => {
+
+    let wrapper = null;
+    beforeEach(() => {
+      wrapper = document.createElement('section');
+      document.body.appendChild(wrapper);
+    });
+
+    afterEach(() => {
+      unmountComponentAtNode(wrapper);
+      wrapper.remove();
+      wrapper = null;
+    });
+
+  });
 
 });
--- src/components/registrationForm/registrationForm.tsx
+++ src/components/registrationForm/registrationForm.tsx
@@ -1,95 +1,100 @@
 import classes from './registrationForm.module.scss'
 import React from "react";
-import { Field, Formik, Form } from 'formik';
-import { validationField } from '../../services/validateForm/ValidatorsField';
-import {FormButton,
-    FormCloseButton,
-    FormInput,
-    FormInputError,
-    FormTextField,
-    FormTitle,} from '../common/formElements'
+import {Field, Formik, Form} from 'formik';
+import {validationField} from '../../services/validateForm/ValidatorsField';
+import {
+  FormButton,
+  FormCloseButton,
+  FormInput,
+  FormInputError,
+  FormTextField,
+  FormTitle,
+} from '../common/formElements'
+//import {useDispatch} from "react-redux";
+//import RegisterUserHook from ',,/../services/RegisterUserHook'
 
 
 const RegistrationForm: React.FC = () => {
+  // const dispatch = useDispatch();
   return (
-      <section className={classes.wrapper}>
-          <div className={classes.form}>
-            <div className={classes.wrapperImg}>
-              <img
-                  src='/assets/images/imgRegistration.svg'
-                   alt='win'/>
-            </div>
-            <FormCloseButton />
-            <FormTitle title='Реєстрація' />
-           {/* <FormInputError errorType='form' errorMessage='Користувач з такою електронною поштою вже зареєстрований'/>*/}
-            <Formik
-                initialValues={{ email: '', password: '', confirmPassword: '' }}
-                validationSchema={validationField}
-                onSubmit={(values, actions) => {
-                  console.log(values);
-                  actions.setSubmitting(false);
-                  actions.resetForm({
-                    values: {
-                      email: '',
-                      password: '',
-                      confirmPassword: '',
-                    },
-                  });
-
-                }}
-            >
-              {({
-                  values,
-                  errors,
-                  touched,
-                  handleChange,
-                  handleBlur,
-                  handleSubmit,
-                  isSubmitting,
-                }) => (
-                  <Form onSubmit={handleSubmit}>
-                    <div>
-                      <Field
-                          component={FormInput}
-                          placeholder={'Електронна пошта'}
-                          iconName ='email'
-                          type="email"
-                          name="email"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.email}
-                      />
-                    </div>
-                    <div>
-                      <Field
-                          component={FormInput}
-                          placeholder={'Пароль'}
-                          iconName ='lock'
-                          type="password"
-                          name="password"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.password}
-                      />
-                    </div>
-                    <div>
-                      <Field
-                          component={FormInput}
-                          placeholder={'Підтвердіть пароль'}
-                          iconName ='lock'
-                          type="password"
-                          name="confirmPassword"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.confirmPassword}
-                      />
-                    </div>
-                    < FormButton data-testid='button' form='register' title='Увійти'/>
-                    <FormTextField text='Вже зареєстровані?' url={'/login'} />
-                  </Form>
-              )}
-            </Formik>
+      <section role='section' className={classes.wrapper}>
+        <div role='wrapper' className={classes.form}>
+          <div className={classes.wrapperImg}>
+            <img
+                src='/assets/images/imgRegistration.svg'
+                alt='win'/>
           </div>
+          <FormCloseButton/>
+          <FormTitle title='Реєстрація'/>
+          <Formik
+              initialValues={{email: '', password: '', confirmPassword: ''}}
+              validationSchema={validationField}
+              onSubmit={(values, actions) => {
+                actions.setSubmitting(false);
+                actions.resetForm({
+                  values: {
+                    email: '',
+                    password: '',
+                    confirmPassword: '',
+                  },
+                });
+                //const { email, password, confirmPassword } = values;
+                //console.log(email, password, confirmPassword);
+                /*dispatch(RegisterUserHook(email, password, confirmPassword));*/
+              }}
+          >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                  <div>
+                    <Field
+                        component={FormInput}
+                        placeholder={'Електронна пошта'}
+                        iconName='email'
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                    />
+                  </div>
+                  <div>
+                    <Field
+                        component={FormInput}
+                        placeholder={'Пароль'}
+                        iconName='lock'
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                    />
+                  </div>
+                  <div>
+                    <Field
+                        component={FormInput}
+                        placeholder={'Підтвердіть пароль'}
+                        iconName='lock'
+                        type="password"
+                        name="confirmPassword"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.confirmPassword}
+                    />
+                  </div>
+                  < FormButton data-testid='button' form='register' title='Увійти'/>
+                  <FormTextField text='Вже зареєстровані?' url={'/login'}/>
+                </Form>
+            )}
+          </Formik>
+        </div>
       </section>
   );
 }
