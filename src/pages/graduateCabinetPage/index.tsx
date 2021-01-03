b36e28bc28fd13c173653e9b53cfd00d4ee90051
Roman Serhiychuk <Rominos7@gmail.com>
2021-01-03 17:00:27 +0200

    changes after pull process

--- src/pages/graduateCabinetPage/index.tsx
+++ src/pages/graduateCabinetPage/index.tsx
@@ -1,23 +1,20 @@
-import React,{Fragment} from 'react'
-import styles from './graduateCabinetPage.module.scss';
+import React, { Fragment } from "react";
+import styles from "./graduateCabinetPage.module.scss";
 import {
-    Header,
-    Footer,
-    UserWorksSpace,
-    UniversityMaps,
-  } from '../../components';
+  Header,
+  Footer,
+  UserWorksSpace,
+  UniversityMaps,
+} from "../../components";
 
-const UserCabinet =()=>{
-    return(
-        <Fragment>
-            <Header></Header>
-               <div className={styles.page}>
-                    <UserWorksSpace/>
-                    <UniversityMaps/>
-               </div>
-            <Footer></Footer>
-        </Fragment>
-    )
-}
+const UserCabinet = () => {
+  return (
+    <Fragment>
+      <Header></Header>
+      <UserWorksSpace />
+      <UniversityMaps />
+      <Footer></Footer>
+    </Fragment>
+  );
+};
 export default UserCabinet;
-
