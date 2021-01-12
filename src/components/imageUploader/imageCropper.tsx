beb266491ba787d194860bdd84bc16217296283d
Romanarkko <roman.ark.ko@gmail.com>
2021-01-12 16:51:58 +0200

    end of design part

--- src/components/imageUploader/imageCropper.tsx
+++ src/components/imageUploader/imageCropper.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import style from './imageCropper.module.scss';
-
 const ImageCropper = (props: any) => {
-  const { loadedImage } = props;
+  const { loadedImage, setLoadedImage } = props;
 
   return (
     <div className={style.imagePrevieContainer}>
-      <img src={loadedImage.data?.toString()} alt='loaded profile' />
-
-      <span className={style.progressBar}>
+      <img
+        src='assets/icons/close.svg'
+        className={style.closeButton}
+        alt='close icon'
+        onClick={() =>
+          setLoadedImage({
+            name: '',
+            size: 0,
+            type: '',
+            data: '',
+          })
+        }
+      />
+      <div className={style.imgHolder}>
+        <img src={loadedImage.data?.toString()} alt='loaded profile' />
+      </div>
+      {/* <span className={style.progressBar}>
         {loadedImage.isUploading && <span>loading...</span>}
-      </span>
+      </span> */}
     </div>
   );
 };
