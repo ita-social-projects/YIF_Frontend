4d31d6ce6da93fb0b499e6ad1c13d6254f0cebd6
Roman Serhiychuk <Rominos7@gmail.com>
2021-01-14 14:37:53 +0200

    merged from dev

--- src/components/filter/filter.test.js
+++ src/components/filter/filter.test.js
@@ -1,48 +1,46 @@
-import React,{Fragment} from 'react'
-import { render} from '@testing-library/react';
-import {unmountComponentAtNode} from "react-dom"
-import { fireEvent} from "@testing-library/react";
+import React, { Fragment } from "react";
+import { render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import { fireEvent } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
-import { store } from '../../store/store';
-import Filter from './filter';
+import { store } from "../../store/store";
+import Filter from "./filter";
 
 let container = null;
 
 const mockHistoryPush = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-    useHistory: () => ({
-      push: mockHistoryPush,
-    }),
-  }));
+jest.mock("react-router-dom", () => ({
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
-
-beforeEach(()=>{
-    container = document.createElement("div");
-    document.body.appendChild(container);
-})
-
-afterEach(()=>{
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-})
-
-it("ckeck filter page",() =>{
-  
-    act(() => {
-        ReactDOM.render(
-        <Provider store={store}>
-          <Filter />
-        </Provider>, container);
-      });
-
-    let button = container.querySelector('button');
-    fireEvent.click(button);
-    expect(mockHistoryPush).toHaveBeenCalledWith('/404'); 
-
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
 
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("ckeck filter page", () => {
+  act(() => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <Filter />
+      </Provider>,
+      container
+    );
+  });
+
+  let button = container.querySelector("button");
+  fireEvent.click(button);
+  expect(mockHistoryPush).toHaveBeenCalledWith("/404");
+});
