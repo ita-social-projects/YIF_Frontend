1975f6340211ebcbfa7878d783d5c4634cd0c4d6
Romanarkko <roman.ark.ko@gmail.com>
2021-01-18 15:21:08 +0200

    merged with changes from  teem

--- src/pages/filterPage/index.tsx
+++ src/pages/filterPage/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Redirect } from "react-router-dom";
-type Props={
-    data?:object; 
-}
 
-class FilterPage extends React.Component<Props,{}>{
-    render(){
-        return(
-            <div>
-                Succsesfully done POST
-            </div>
-            
-        )
-    }
+type Props = {
+  data?: object;
+};
 
+class FilterPage extends React.Component<Props, {}> {
+  render() {
+    return <div>Succsesfully done POST</div>;
+  }
 }
 export default FilterPage;
-
