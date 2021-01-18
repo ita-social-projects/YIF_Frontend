f69f2f6ca59fee87aef3c6e74df786c56bafb20e
Andrew Melnychuk <a.melnychuk93@gmail.com>
2021-01-18 14:24:11 +0200

    add protected and redirect routes, fix bugs

--- src/services/useProfile.tsx
+++ src/services/useProfile.tsx
@@ -58,11 +58,7 @@ const useProfile = (endpoint: string) => {
     e.preventDefault();
     setSubmitted(true);
     setError({ hasError: false, errorStatusCode: '', errorMessage: '' });
-    console.log(isExpired);
-    console.log(`before --- ${token}`);
-
     getToken();
-    console.log(`after --- ${token}`);
     requestSecureData(endpoint, 'POST', token!, {
       name,
       surname,
@@ -75,10 +71,10 @@ const useProfile = (endpoint: string) => {
         const statusCode = res.statusCode.toString();
         if (statusCode.match(/^[23]\d{2}$/)) {
           setError({ hasError: false, errorStatusCode: '', errorMessage: '' });
-          setSubmitted(false);
           console.log('success kinda');
           localStorage.setItem('user', JSON.stringify(res.data));
           getUserProfile();
+          setSubmitted(false);
         } else {
           setError({
             hasError: true,
