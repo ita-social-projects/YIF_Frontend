485e4a33f8d1c9893635fdca71908e6babe64ce1
Romanarkko <roman.ark.ko@gmail.com>
2021-02-08 14:15:37 +0200

    added id

--- src/errorBoundry/errorBoundry.test.tsx
+++ src/errorBoundry/errorBoundry.test.tsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import { Provider, useSelector, useDispatch } from 'react-redux';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { store } from '../store/store';
-import requestStatusReducer, {
-  requestStatusState,
-  requestFailure,
-} from '../store/reducers/requestStatus';
 import ErrorBoundry from './index';
-import { configureStore } from '@reduxjs/toolkit';
 
 class TestPage extends Component {
   render() {
@@ -29,37 +24,3 @@ describe('ERROR BOUNDARY', () => {
     expect(testPage).toBeInTheDocument();
   });
 });
-
-// const emptyObj: any = {};
-// const store2 = configureStore({
-//   reducer: {
-//     requestStatus: requestStatusReducer,
-//   },
-// });
-
-// const TestErrorPage = () => {
-//   const dispatch = useDispatch();
-
-//   return (
-//     <span
-//       onClick={() => {
-//         dispatch(requestFailure());
-//       }}
-//       data-testid='errorButton'
-//     >
-//       Error Button
-//     </span>
-//   );
-// };
-
-// test('should catching an error ', () => {
-//   const container = render(
-//     <Provider store={store2}>
-//       <ErrorBoundry>
-//         <TestErrorPage />
-//       </ErrorBoundry>
-//     </Provider>
-//   );
-//   fireEvent.click(screen.getByText('Error Button'));
-//   expect(store2.getState().requestStatus.error).toBeTruthy();
-// });
