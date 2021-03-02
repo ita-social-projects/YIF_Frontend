136f32bc71e244629649088d2e6f16d6d710b01d
KostiantynKukh <kkuh1993@gmail.com>
2021-03-02 12:54:53 +0200

    made task #95

--- src/pages/graduateCabinetPage/index.tsx
+++ src/pages/graduateCabinetPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 //import styles from './graduateCabinetPage.module.scss';
 import {
   Header,
@@ -6,13 +6,34 @@ import {
   UserWorksSpace,
   UniversityMap,
 } from '../../components';
+import { requestData } from '../../services/requestDataFunction';
+import { APIUrl } from '../../services/endpoints';
 
 const UserCabinet = () => {
+  const [universitiesList, setList] = useState([{}]);
+
+  useEffect(() => {
+    const endpoint: string = `${APIUrl}University`;
+
+    requestData(endpoint, 'GET').then((res: any) => {
+      const filteredList = res.data.responseList.map((item: any) => {
+        return {
+          id: item.id,
+          name: item.name,
+          site: item.site,
+          lat: item.lat,
+          lon: item.lon,
+        };
+      });
+      setList(filteredList);
+    });
+  }, [APIUrl]);
+
   return (
     <Fragment>
       <Header />
       <UserWorksSpace />
-      <UniversityMap />
+      <UniversityMap data={universitiesList} />
       <Footer />
     </Fragment>
   );
