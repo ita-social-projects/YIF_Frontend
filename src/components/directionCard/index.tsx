db0d19baca31549d492249c30b7801eb873754ac
Vadym Ilchenko <62015447+v-ilchenko@users.noreply.github.com>
2021-03-09 14:56:13 +0300

    fixed bug with changed key_names in Directions response (#108)

--- src/components/directionCard/index.tsx
+++ src/components/directionCard/index.tsx
@@ -12,15 +12,15 @@ interface Props {
 const DirectionCard: React.FC<Props> = (props) => {
   let { code, name, description, specialties } = props;
 
-  const result = specialties
-    .sort((a: any, b: any) => a.code - b.code)
+  const specialtiesList = specialties
+    .sort((a: any, b: any) => a.specialtyCode - b.specialtyCode)
     .map((item: any) => (
       <li key={item.id} className={styles.card__content__list__subitem}>
         <div className={styles.card__content__list__subitem_info}>
-          <span>{item.code}</span>
-          <h5>{item.name}</h5>
+          <span>{item.specialtyCode}</span>
+          <h5>{item.specialtyName}</h5>
         </div>
-        <Link to={`/specialty/${item.id}`}>Детальніше</Link>
+        <Link to={`/specialty/${item.specialtyId}`}>Детальніше</Link>
       </li>
     ));
 
@@ -39,7 +39,7 @@ const DirectionCard: React.FC<Props> = (props) => {
         <div className={styles.card__content__wrapper}>
           <p className={styles.card__content__desc}>{description}</p>
           <h3 className={styles.card__content__subtitle}>Спеціальності:</h3>
-          <ul className={styles.card__content__list}>{result}</ul>
+          <ul className={styles.card__content__list}>{specialtiesList}</ul>
         </div>
       </div>
     </div>
