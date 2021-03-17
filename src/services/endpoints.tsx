60107a5dfdceee0984ba6275566c276e2935266e
Parchuk <sergeyparchuk1@gmail.com>
2021-03-17 13:41:04 +0200

    chenged endpoint

--- src/services/endpoints.tsx
+++ src/services/endpoints.tsx
@@ -1,5 +1,5 @@
-
-export const APIUrl: string = 'https://yifbackend.tk/api/';
+export const APIUrl: string = 'https://yifbackend.herokuapp.com/api/';
+// export const APIUrl: string = 'https://yifbackend.tk/api/';
 //export const APIUrl: string = 'https://localhost:44324/api/';
 //export const APIUrl: string = 'http://localhost:5000/api/';
 //export const APIUrl: string = 'https://web-yifbackend.azurewebsites.net/api/';
