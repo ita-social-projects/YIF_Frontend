f472532dd061772a638a7c3fcc006824a9e303f6
Vadym Ilchenko <vadim.ilchenko@gmail.com>
2021-04-12 00:06:22 +0300

    tests 100%

--- src/components/forCabinetPage/options/institutionOfEducationListOption/institutionOfEducationListOption.test.js
+++ src/components/forCabinetPage/options/institutionOfEducationListOption/institutionOfEducationListOption.test.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import ReactDOM, { unmountComponentAtNode } from 'react-dom';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import InstitutionOfEducationListOption from './index.tsx';
 import { Provider } from 'react-redux';
 import { store } from '../../../../store/store.ts';
-import {
-  cleanup,
-  act,
-  fireEvent,
-  queryAllByTestId,
-  queryByTestId,
-} from '@testing-library/react';
-import { authContext } from '../../../../services/tokenValidator';
-import { createMemoryHistory } from 'history';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 
-let container = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+jest.mock('../../../../services/tokenValidator', () => {
+  return {
+    useAuth: () => {
+      return {
+        getToken: jest.fn(() => '123'),
+      };
+    },
+  };
 });
 
-afterEach(cleanup);
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 const data = [
   {
-    liked: true,
+    isFavorite: true,
     id: 'sdsfsf',
     abbreviation: 'abbreviation1',
     site: 'site1',
@@ -33,6 +30,9 @@ const data = [
       'long description1 long description1 long description1 long description1 long description1 long description1long description1 long description1 long description1 long description1 long description1 long description1long description1long description1 long description1',
     startOfCampaign: 'startOfCampaign1',
     endOfCampaign: 'endOfCampaign1',
+    handleClick: () => {
+      console.log(`data`);
+    },
   },
 ];
 
@@ -44,102 +44,116 @@ const mockFetchPromise = Promise.resolve({
 });
 global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 
-const history = createMemoryHistory();
-
-it('check success response', async () => {
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
+describe('test IOEFavoritesList component', () => {
+  test('check success response', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
             <InstitutionOfEducationListOption />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
+          </Router>
+        </Provider>
+      );
+    });
+
+    const titles = document.querySelectorAll('h2');
+    expect(titles).toHaveLength(1);
   });
 
-  const titles = container.querySelectorAll('h2');
-  expect(titles).toHaveLength(1);
-});
+  test('check error ', async () => {
+    const mockFetchPromiseError = Promise.resolve({
+      json: () => mockJsonPromise,
+      status: 404,
+    });
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseError);
 
-it('check error ', async () => {
-  const mockFetchPromiseError = Promise.resolve({
-    json: () => mockJsonPromise,
-    status: 404,
-  });
-  global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseError);
-
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
             <InstitutionOfEducationListOption />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
+          </Router>
+        </Provider>
+      );
+    });
+
+    const titles = document.querySelectorAll('h3');
+    expect(titles).toHaveLength(1);
   });
 
-  const titles = container.querySelectorAll('h3');
-  console.log(titles[0].innerHTML);
-  expect(titles).toHaveLength(1);
-});
+  test('click on the star deleting from favorites', async () => {
+    const mockJsonPromiseStarError = Promise.resolve(data);
+    const mockFetchPromiseStarError = Promise.resolve({
+      json: () => mockJsonPromiseStarError,
+      status: 200,
+    });
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() => mockFetchPromiseStarError);
 
-it('click on the star deleting from favorites', async () => {
-  const mockJsonPromiseStarError = Promise.resolve(data);
-  const mockFetchPromiseStarError = Promise.resolve({
-    json: () => mockJsonPromiseStarError,
-    status: 200,
-  });
-  global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseStarError);
-
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
             <InstitutionOfEducationListOption />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
+          </Router>
+        </Provider>
+      );
+    });
+
+    const star = screen.getByTestId('star');
+
+    await act(async () => {
+      fireEvent.click(star);
+    });
+
+    expect(fetch).toBeCalledTimes(3);
   });
 
-  const star = queryByTestId(container, 'star');
-  fireEvent.click(star);
+  test('click on the star with bad response ', async () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <InstitutionOfEducationListOption />
+        </Router>
+      </Provider>
+    );
+    await act(async () => {});
+    const star = screen.getAllByTestId('star');
+    global.fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => 'bad news',
+        status: 404,
+      })
+    );
+    await act(async () => {
+      fireEvent.click(star[0]);
+    });
+    const placeholder = screen.getByTestId('placeholder');
+    expect(fetch).toBeCalledTimes(3);
+    expect(placeholder).toBeInTheDocument();
+  });
+
+  test('click on the star with no response', async () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <InstitutionOfEducationListOption />
+        </Router>
+      </Provider>
+    );
+    await act(async () => {});
+    const star = screen.getAllByTestId('star');
+    global.fetch.mockImplementationOnce(() =>
+      Promise.reject({
+        json: () => 'bad news',
+        status: 404,
+      })
+    );
+    await act(async () => {
+      fireEvent.click(star[0]);
+    });
+    const placeholder = screen.getByTestId('placeholder');
+    expect(placeholder).toBeInTheDocument();
+  });
 });
--- src/pages/institutionsOfEducationListPage/InstitutionsOfEducationListPage.test.js
+++ src/pages/institutionsOfEducationListPage/InstitutionsOfEducationListPage.test.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import InstitutionsOfEducationListPage from '.';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
-import {
-  cleanup,
-  act,
-  fireEvent,
-  queryAllByTestId,
-  queryByTestId,
-} from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
-import { authContext } from '../../services/tokenValidator';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-let container = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
-afterEach(cleanup);
-
-const data = {
+let data = {
   currentPage: 3,
   totalPages: 3,
   responseList: [
     {
-      isFavorite: false,
-      id: 'sdsfsf',
+      isFavorite: true,
+      id: '1',
       abbreviation: 'abbreviation1',
       site: 'site1',
       address: 'address1',
@@ -38,7 +25,7 @@ const data = {
     },
     {
       isFavorite: false,
-      id: 'dfijfjenvnciebv',
+      id: '2',
       abbreviation: 'abbreviation2',
       site: 'site2',
       address: 'address2',
@@ -49,7 +36,7 @@ const data = {
     },
     {
       isFavorite: false,
-      id: 'fsdfsdfsdf',
+      id: '3',
       abbreviation: 'abbreviation3',
       site: 'site3',
       address: 'address3',
@@ -67,276 +54,289 @@ const mockFetchPromise = Promise.resolve({
 });
 global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 
-it('check success response', async () => {
-  const history = createMemoryHistory();
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
-            <InstitutionsOfEducationListPage />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
-  });
+const mock = require('../../services/tokenValidator');
 
-  const cards = queryAllByTestId(container, 'card');
-  expect(cards).toHaveLength(3);
-
-  const heading = queryByTestId(container, 'heading');
-  expect(heading.innerHTML).toBe('Заклади освіти');
-
-  const pageButtons = queryAllByTestId(container, 'currentPage');
-  fireEvent.click(pageButtons[2]);
-  const prevButton = queryByTestId(container, 'prevPage');
-  fireEvent.click(prevButton);
-  const nextButton = queryByTestId(container, 'nextPage');
-  fireEvent.click(nextButton);
-});
-
-it('check error ', async () => {
-  const mockFetchPromiseError = Promise.resolve({
-    json: () => mockJsonPromise,
-    status: 404,
-  });
-  global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseError);
-
-  const history = createMemoryHistory();
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: '',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
-            <InstitutionsOfEducationListPage />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
-  });
-
-  const placeholder = queryByTestId(container, 'placeholder');
-  expect(placeholder).toBeInTheDocument();
-});
-
-it('history', async () => {
-  const history = createMemoryHistory();
-  const state = { id: 'directionId' };
-  history.push('/', state);
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
-            <InstitutionsOfEducationListPage />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
-  });
-});
-
-it('check pagination with total pages = 1', async () => {
-  const history = createMemoryHistory();
-  const dataPagination = {
-    currentPage: 1,
-    totalPages: 1,
-    responseList: [
-      {
-        isFavorite: false,
-        id: 'sdsfsf',
-        abbreviation: 'abbreviation1',
-        site: 'site1',
-        address: 'address1',
-        description: 'description1',
-        startOfCampaign: 'startOfCampaign1',
-        endOfCampaign: 'endOfCampaign1',
-      },
-    ],
+mock.useAuth = jest.fn(() => {
+  return {
+    token: 'token',
+    getToken: jest.fn(() => '123'),
   };
-  const mockJsonPromisePagination = Promise.resolve(dataPagination);
-
-  const mockFetchPromisePagination = Promise.resolve({
-    json: () => mockJsonPromisePagination,
-    status: 200,
-  });
-  global.fetch = jest.fn().mockImplementation(() => mockFetchPromisePagination);
-
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {},
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
-            <InstitutionsOfEducationListPage />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
-  });
-
-  const prevButton = queryByTestId(container, 'prevPage');
-  fireEvent.click(prevButton);
-  const nextButton = queryByTestId(container, 'nextPage');
-  fireEvent.click(nextButton);
-  const pageButton = queryByTestId(container, 'currentPage');
-  fireEvent.click(pageButton);
 });
 
-it('click on the star adding to favorites', async () => {
-  const history = createMemoryHistory();
-  const currentToken = 'Token';
-
-  const dataPagination = {
-    currentPage: 1,
-    totalPages: 1,
-    responseList: [
-      {
-        isFavorite: false,
-        id: 'sdsfsf',
-        abbreviation: 'abbreviation1',
-        site: 'site1',
-        address: 'address1',
-        description: 'description1',
-        startOfCampaign: 'startOfCampaign1',
-        endOfCampaign: 'endOfCampaign1',
-      },
-    ],
-  };
-
-  const mockJsonPromiseStar = Promise.resolve(dataPagination);
-
-  const mockFetchPromiseStar = Promise.resolve({
-    json: () => mockJsonPromiseStar,
-    status: 200,
-  });
-  global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseStar);
-
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {
-                return currentToken;
-              },
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
+describe('test IOEFavoritesList component', () => {
+  test('check success response', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
             <InstitutionsOfEducationListPage />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
+          </Router>
+        </Provider>
+      );
+    });
+
+    const cards = screen.getAllByTestId('card');
+    expect(cards).toHaveLength(3);
+
+    const heading = screen.getByTestId('heading');
+    expect(heading.innerHTML).toBe('Заклади освіти');
+
+    const pageButtons = screen.getAllByTestId('currentPage');
+    await act(async () => {
+      fireEvent.click(pageButtons[2]);
+    });
+    const prevButton = screen.getByTestId('prevPage');
+    await act(async () => {
+      fireEvent.click(prevButton);
+    });
+
+    const nextButton = screen.getByTestId('nextPage');
+    await act(async () => {
+      fireEvent.click(nextButton);
+    });
   });
 
-  const star = queryByTestId(container, 'star');
-  fireEvent.click(star);
-});
+  test('check error ', async () => {
+    const mockFetchPromiseError = Promise.resolve({
+      json: () => mockJsonPromise,
+      status: 404,
+    });
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseError);
 
-it('click on the star deleting from favorites', async () => {
-  const history = createMemoryHistory();
-  const currentToken = 'Token';
-
-  const dataPagination = {
-    currentPage: 1,
-    totalPages: 1,
-    responseList: [
-      {
-        isFavorite: true,
-        id: 'sdsfsf',
-        abbreviation: 'abbreviation1',
-        site: 'site1',
-        address: 'address1',
-        description: 'description1',
-        startOfCampaign: 'startOfCampaign1',
-        endOfCampaign: 'endOfCampaign1',
-      },
-    ],
-  };
-  const mockJsonPromiseStarDel = Promise.resolve(dataPagination);
-
-  const mockFetchPromiseStarDel = Promise.resolve({
-    json: () => mockJsonPromiseStarDel,
-    status: 200,
-  });
-  global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseStarDel);
-
-  await act(async () => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={history}>
-          <authContext.Provider
-            value={{
-              token: 'testToken',
-              refreshToken: 'Token',
-              isExpired: false,
-              isRefreshing: false,
-              getToken: () => {
-                return currentToken;
-              },
-              updateToken: () => {},
-              removeToken: () => {},
-            }}
-          >
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
             <InstitutionsOfEducationListPage />
-          </authContext.Provider>
-        </Router>
-      </Provider>,
-      container
-    );
+          </Router>
+        </Provider>
+      );
+    });
+
+    const placeholder = screen.getByTestId('placeholder');
+    expect(placeholder).toBeInTheDocument();
   });
 
-  const star = queryByTestId(container, 'star');
-  fireEvent.click(star);
+  test('check pagination with total pages = 1', async () => {
+    data = {
+      currentPage: 1,
+      totalPages: 1,
+      responseList: [
+        {
+          isFavorite: false,
+          id: '1',
+          abbreviation: 'abbreviation1',
+          site: 'site1',
+          address: 'address1',
+          description: 'description1',
+          startOfCampaign: 'startOfCampaign1',
+          endOfCampaign: 'endOfCampaign1',
+        },
+      ],
+    };
+    const mockJsonPromisePagination = Promise.resolve(data);
+
+    const mockFetchPromisePagination = Promise.resolve({
+      json: () => mockJsonPromisePagination,
+      status: 200,
+    });
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() => mockFetchPromisePagination);
+
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
+            <InstitutionsOfEducationListPage />
+          </Router>
+        </Provider>
+      );
+    });
+
+    const prevButton = screen.getByTestId('prevPage');
+    await act(async () => {
+      fireEvent.click(prevButton);
+    });
+
+    const nextButton = screen.getByTestId('nextPage');
+    await act(async () => {
+      fireEvent.click(nextButton);
+    });
+
+    const pageButton = screen.getByTestId('currentPage');
+    await act(async () => {
+      fireEvent.click(pageButton);
+    });
+  });
+
+  test('click on the star adding to favorites', async () => {
+    data = {
+      currentPage: 1,
+      totalPages: 1,
+      responseList: [
+        {
+          liked: false,
+          id: '1',
+          abbreviation: 'abbreviation1',
+          site: 'site1',
+          address: 'address1',
+          description: 'description1',
+          startOfCampaign: 'startOfCampaign1',
+          endOfCampaign: 'endOfCampaign1',
+        },
+        {
+          liked: false,
+          id: '2',
+          abbreviation: 'abbreviation1',
+          site: 'site1',
+          address: 'address1',
+          description: 'description1',
+          startOfCampaign: 'startOfCampaign1',
+          endOfCampaign: 'endOfCampaign1',
+        },
+      ],
+    };
+
+    const mockJsonPromiseStar = Promise.resolve(data);
+
+    const mockFetchPromiseStar = Promise.resolve({
+      json: () => mockJsonPromiseStar,
+      status: 200,
+    });
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseStar);
+
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
+            <InstitutionsOfEducationListPage />
+          </Router>
+        </Provider>
+      );
+    });
+
+    const star = screen.getAllByTestId('star');
+    await act(async () => {
+      fireEvent.click(star[1]);
+    });
+
+    expect(fetch).toBeCalledTimes(2);
+  });
+
+  test('click on the star deleting from favorites', async () => {
+    data = {
+      currentPage: 1,
+      totalPages: 1,
+      responseList: [
+        {
+          isFavorite: true,
+          id: '1',
+          abbreviation: 'abbreviation1',
+          site: 'site1',
+          address: 'address1',
+          description: 'description1',
+          startOfCampaign: 'startOfCampaign1',
+          endOfCampaign: 'endOfCampaign1',
+        },
+      ],
+    };
+    const mockJsonPromiseStarDel = Promise.resolve(data);
+
+    const mockFetchPromiseStarDel = Promise.resolve({
+      json: () => mockJsonPromiseStarDel,
+      status: 200,
+    });
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromiseStarDel);
+
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
+            <InstitutionsOfEducationListPage />
+          </Router>
+        </Provider>
+      );
+    });
+
+    const star = screen.getByTestId('star');
+
+    await act(async () => {
+      fireEvent.click(star);
+    });
+
+    expect(fetch).toBeCalledTimes(2);
+  });
+
+  test('click on the star with bad response ', async () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <InstitutionsOfEducationListPage />
+        </Router>
+      </Provider>
+    );
+    await act(async () => {});
+    const star = screen.getAllByTestId('star');
+    global.fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => 'bad news',
+        status: 404,
+      })
+    );
+    await act(async () => {
+      fireEvent.click(star[0]);
+    });
+    const placeholder = screen.getByTestId('placeholder');
+    expect(fetch).toBeCalledTimes(3);
+    expect(placeholder).toBeInTheDocument();
+  });
+
+  test('click on the star with no response', async () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <InstitutionsOfEducationListPage />
+        </Router>
+      </Provider>
+    );
+    await act(async () => {});
+    const star = screen.getAllByTestId('star');
+    global.fetch.mockImplementationOnce(() =>
+      Promise.reject({
+        json: () => 'bad news',
+        status: 404,
+      })
+    );
+    await act(async () => {
+      fireEvent.click(star[0]);
+    });
+    const placeholder = screen.getByTestId('placeholder');
+    expect(placeholder).toBeInTheDocument();
+  });
+
+  test('nonauthorized user render', async () => {
+    mock.useAuth = jest.fn(() => {
+      return {
+        token: '',
+        getToken: jest.fn(() => undefined),
+      };
+    });
+
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
+            <InstitutionsOfEducationListPage />
+          </Router>
+        </Provider>
+      );
+    });
+
+    const star = screen.getAllByTestId('star');
+
+    await act(async () => {
+      fireEvent.click(star[0]);
+    });
+  });
 });
