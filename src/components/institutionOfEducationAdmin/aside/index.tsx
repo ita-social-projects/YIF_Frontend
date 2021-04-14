ea48c97687e33513527478eefec9a08e362e0247
Andrii Sharun <andrii.sharun.dev@gmail.com>
2021-04-14 20:29:57 +0300

    Change function declaration

--- src/components/institutionOfEducationAdmin/aside/index.tsx
+++ src/components/institutionOfEducationAdmin/aside/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import styles from './aside.module.scss';
 
-function Aside({ children }: any) {
+const Aside = ({ children }: any) => {
   return (
     <aside className={styles.assideMenu}>
       <div className={styles.navContainer}>{children}</div>
     </aside>
   );
-}
+};
 
 export default Aside;
