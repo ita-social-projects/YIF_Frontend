c670a2706cce225b91c5b796fc14f1974d5d64ac
Tetiana <ostannja@gmail.com>
2021-06-10 12:13:47 +0300

    pagination test coverage 80+%

--- src/components/pagination/pagination.test.js
+++ src/components/pagination/pagination.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import Pagination from '.';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 const paginationPage = {
   totalPages: '2',
@@ -21,12 +25,25 @@ describe('Pagination', () => {
           <Pagination
             totalPages={totalPages}
             currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
+            setCurrentPage={() => setCurrentPage}
             pages={pages}
           />
         </Provider>
       </Router>
     );
     expect(screen.getAllByText('2')).toHaveLength(1);
+
+    const nextButton = screen.getByTestId('nextPage');
+    fireEvent.click(nextButton);
+    expect(nextButton).not.toHaveClass('arrowUnable');
+
+    const prevButton = screen.getByTestId('prevPage');
+    fireEvent.click(prevButton);
+
+    const pageButton1 = screen.getByText('2');
+    fireEvent.click(pageButton1);
+
+    const pageButton2 = screen.getByText('1');
+    fireEvent.click(pageButton2);
   });
 });
