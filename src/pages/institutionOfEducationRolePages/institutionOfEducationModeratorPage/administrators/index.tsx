9f4d30046c5621c5d3bd3f58bf692aa13c59ad0c
Anton Novak <novak.anton.mailbox@gmail.com>
2021-06-10 22:37:33 +0300

    raised addSpecialties component test coverage to 100%

---
+++ src/pages/institutionOfEducationRolePages/institutionOfEducationModeratorPage/administrators/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import Administrator from '../../../../components/institutionOfEducationModerator/administrator';
+import styles from './administrators.module.scss';
+
+interface Administrator {
+  id: number;
+  email: string;
+}
+
+const administrators = [
+  {
+    id: 1,
+    email: 'administrator1@gmail.com'
+  },
+  {
+    id: 2,
+    email: 'administrator2@gmail.com'
+  },
+  {
+    id: 3,
+    email: 'administrator3@gmail.com'
+  }
+];
+
+function Administrators() {
+
+  const [administratorsState, setAdministratorsState] = useState<Array<Administrator>>([
+    {
+      id: 0,
+      email: ''
+    },
+  ]);
+
+  useEffect(() => {
+    setAdministratorsState(administrators);
+  }, []);
+
+  return (
+    <main className={styles.administrators}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Адміністратори</h1>
+        <div className={styles.administratorList}>
+          {administratorsState.map((administrator) => {
+            const { id, email } = administrator;
+            return (
+              <Administrator
+                key={id}
+                email={email}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Administrators;
---
+++ src/pages/institutionOfEducationRolePages/institutionOfEducationModeratorPage/institutionOfEducationModeratorPage.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InstitutionOfEducationModeratorPage from './index';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { store } from '../../../store/store';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MemoryRouter>
+      <Provider store={store}>
+        <InstitutionOfEducationModeratorPage />
+      </Provider>
+    </MemoryRouter>,
+    div
+  );
+});
