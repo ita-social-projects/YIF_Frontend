7e1a53d9e22307195f2a8ad0cb4cbf301ee1a479
Anton Novak <novak.anton.mailbox@gmail.com>
2021-06-10 23:17:13 +0300

    changed file structure

---
+++ src/pages/institutionOfEducationAdminPage/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Footer, Header } from '../../components';
+import Aside from '../../components/institutionOfEducationAdmin/aside';
+import InstitutionOfEducationInfo from './institutionOfEducationInfo';
+import styles from './institutionOfEducationAdminPage.module.scss';
+import { Route, Redirect, Switch, useRouteMatch, Link } from 'react-router-dom';
+import Moderators from './moderators';
+import EditInstitutionOfEducationInfoPage from './editInstitutionOfEducationInfoPage';
+import OurSpecialties from './ourSpecialties';
+import EditSpecialty from './editSpecialtyPage';
+import AddSpecialties from '../institutionOfEducationModeratorPage/addSpecialties';
+import ChangePassword from '../../components/changePassword';
+
+function InstitutionOfEducationAdminPage() {
+  const { path } = useRouteMatch();
+  return (
+    <>
+      <Header />
+      <section className={styles.institutionOfEducationAdminContent}>
+        <Aside>
+          <nav>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/institutionOfEducationInfo`}
+            >
+              Заклад освіти
+            </Link>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/ourSpecialties`}
+            >
+              Спеціальності
+            </Link>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/moderators`}
+            >
+              Модератори
+            </Link>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/changePassword`}
+            >
+              Змінити пароль
+            </Link>
+          </nav>
+        </Aside>
+        <Switch>
+          <Route exact path={`/institutionOfEducationAccount`}>
+            <Redirect to={`${path}/ourSpecialties`} />
+          </Route>
+
+          <Route exact path={`${path}/institutionOfEducationInfo`}>
+            <InstitutionOfEducationInfo />
+          </Route>
+
+          <Route path={`${path}/institutionOfEducationInfo/edit`}>
+            <EditInstitutionOfEducationInfoPage />
+          </Route>
+
+          <Route exact path={`${path}/ourSpecialties`}>
+            <OurSpecialties />
+          </Route>
+          <Route path={`${path}/addSpecialties`}>
+            <AddSpecialties />
+          </Route>
+          <Route path={`${path}/ourSpecialties/edit`}>
+            <EditSpecialty />
+          </Route>
+          <Route path={`${path}/moderators`}>
+            <Moderators />
+          </Route>
+          <Route exact path={`${path}/changePassword`}>
+            <ChangePassword />
+          </Route>
+        </Switch>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default InstitutionOfEducationAdminPage;
---
+++ src/pages/institutionOfEducationModeratorPage/index.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Footer, Header } from '../../components';
+import Aside from '../../components/institutionOfEducationAdmin/aside';
+import InstitutionOfEducationInfo from './institutionOfEducationInfo';
+import styles from './institutionOfEducationModeratorPage.module.scss';
+import { Route, Redirect, Switch, useRouteMatch, Link } from 'react-router-dom';
+import OurSpecialties from './ourSpecialties';
+import EditSpecialty from './editSpecialtyPage';
+import AddSpecialties from './addSpecialties';
+import ChangePassword from '../../components/changePassword';
+import Administrators from './administrators'
+
+function InstitutionOfEducationModeratorPage() {
+  const { path } = useRouteMatch();
+  return (
+    <>
+      <Header />
+      <section className={styles.institutionOfEducationModeratorContent}>
+        <Aside>
+          <nav>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/institutionOfEducationInfo`}
+            >
+              Заклад освіти
+            </Link>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/ourSpecialties`}
+            >
+              Спеціальності
+            </Link>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/administrators`}
+            >
+              Адміністратори
+            </Link>
+            <Link
+              className={styles.underlineAnimation}
+              to={`${path}/changePassword`}
+            >
+              Змінити пароль
+            </Link>
+          </nav>
+        </Aside>
+        <Switch>
+          <Route exact path={`/IoEModeratorAccount`}>
+            <Redirect to={`${path}/ourSpecialties`} />
+          </Route>
+
+          <Route exact path={`${path}/institutionOfEducationInfo`}>
+            <InstitutionOfEducationInfo />
+          </Route>
+
+          <Route exact path={`${path}/ourSpecialties`}>
+            <OurSpecialties />
+          </Route>
+          <Route path={`${path}/administrators`}>
+            <Administrators />
+          </Route>
+          <Route path={`${path}/addSpecialties`}>
+            <AddSpecialties />
+          </Route>
+          <Route path={`${path}/ourSpecialties/edit`}>
+            <EditSpecialty />
+          </Route>
+          <Route exact path={`${path}/changePassword`}>
+            <ChangePassword />
+          </Route>
+        </Switch>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default InstitutionOfEducationModeratorPage;
