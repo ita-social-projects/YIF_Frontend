9b8681532bda75aaabbb0560b02bddc01609a3e2
Anton Novak <aantik802@gmail.com>
2021-06-28 15:00:28 +0300

    Added Universities page info rendering instead of data mocking

--- src/components/superAdmin/universityItem/index.tsx
+++ src/components/superAdmin/universityItem/index.tsx
@@ -3,35 +3,33 @@ import Edit from '../../common/icons/Edit';
 import Unlock from '../../common/icons/Unlock';
 import Lock from '../../common/icons/Lock';
 import styles from './universityItem.module.scss';
+import { Link } from 'react-router-dom';
 
 interface Props {
   abbreviation: string;
   fullName: string;
-  isBlocked: boolean;
-  handleBlocking(): void;
-  handleEditing(): void;
+  IoEid: string
 }
 
 const UniversityItem: React.FC<Props> = ({
-  handleEditing,
-  handleBlocking,
-  isBlocked,
-  fullName,
-  abbreviation,
+  fullName, abbreviation, IoEid
 }) => {
   let blockIcon;
-  if (isBlocked) {
-    blockIcon = <Lock handleClick={handleBlocking} />;
-  } else {
-    blockIcon = <Unlock handleClick={handleBlocking} />;
-  }
+  // if (isBlocked) {
+  //   blockIcon = <Lock handleClick={()=>{}} />;
+  // } else {
+  //   blockIcon = <Unlock handleClick={()=>{}} />;
+  // }
   return (
     <div className={styles.container}>
       <div className={styles.abreviation}>{abbreviation}</div>
       <div className={styles.fullName}>{fullName}</div>
       <div className={styles.actionItems}>
         {blockIcon}
-        <Edit handleClick={handleEditing} />
+        <Link to={{
+        pathname: '/superAdminAccount/addInstitutionOfEducationAdmin',
+        state: {IoEid: IoEid}
+      }}><Edit/></Link>
       </div>
     </div>
   );
--- src/pages/superAdminPages/universityListPage/index.tsx
+++ src/pages/superAdminPages/universityListPage/index.tsx
@@ -1,44 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Pagination from '../../../components/superAdmin/pagination';
 import SortingPanel from '../../../components/superAdmin/sortingPanel';
 import UniversityItem from '../../../components/superAdmin/universityItem';
 import styles from './universityListPage.module.scss';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { APIUrl } from '../../../services/endpoints';
+import { requestSecureData } from '../../../services/requestDataFunction';
+import { useAuth } from '../../../services/tokenValidator';
 
-const univList = [
-  {
-    id: '1',
-    abbreviation: 'НУВГП',
-    name:
-      'Національний університет водного господарства та природокористування',
-    isBlocked: false,
-  },
-  {
-    id: '2',
-    abbreviation: 'РДГУ',
-    name: 'Рівненський державний гуманітарний університет',
-    isBlocked: true,
-  },
-  {
-    id: '3',
-    abbreviation: 'ОА',
-    name: 'Національний університет "Острозька академія"',
-    isBlocked: false,
-  },
-  {
-    id: '4',
-    abbreviation: 'НАВС',
-    name: 'Академія внутрішніх військ МВС України',
-    isBlocked: false,
-  },
-  {
-    id: '5',
-    abbreviation: 'КПІ',
-    name: 'Київський політехнічний інститут імені Ігоря Сікорського',
-    isBlocked: true,
-  },
-];
+interface University {
+  id: string,
+  name: string,
+  abbreviation: string,
+  site: string,
+  address: string,
+  phone: string,
+  email: string,
+  description: string,
+  imagePath: string,
+}
+
+interface Response {
+  responseList: University[]
+}
 
 const UniversityListPage: React.FC = () => {
+
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(false);
+  const { path } = useRouteMatch();
+  const { getToken } = useAuth();
+  const [ response, setData ] = useState<Response>({
+    responseList: [{
+      id: '',
+      name: '',
+      abbreviation: '',
+      site: '',
+      address: '',
+      phone: '',
+      email: '',
+      description: '',
+      imagePath: '',
+    }]
+  });
+
+  useEffect(() => {
+    const getInfo = async () => {
+      try {
+        const currentToken = await getToken();
+        const { statusCode, data }: any = await requestSecureData(
+          `${APIUrl}InstitutionOfEducation/Anonymous?InstitutionOfEducationType=0&page=1&pageSize=10`,
+          'GET',
+          currentToken,
+        );
+        if (statusCode.toString().match(/^[23]\d{2}$/)) {
+          setData(data);
+          setError(false);
+        } else {
+          setError(true);
+        }
+      } catch (e) {
+        console.log(e);
+        setError(true);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+    getInfo();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -46,19 +76,24 @@ const UniversityListPage: React.FC = () => {
           <h1>Університети</h1>
           <Pagination />
           <SortingPanel />
-          {univList.map((university) => {
-            const { id, name, abbreviation, isBlocked } = university;
+          {response.responseList.map((university) => {
+            const { id, name, abbreviation } = university;
             return (
               <UniversityItem
                 key={id}
                 abbreviation={abbreviation}
                 fullName={name}
-                isBlocked={isBlocked}
-                handleBlocking={() => {}}
-                handleEditing={() => {}}
+                IoEid={id}
               />
             );
           })}
+          <Link to={`/superAdminAccount/addInstitutionOfEducation`}>
+            <button
+              className={`${styles.addButton} ${styles.animatedButton}`}
+            >
+              Додати заклад освіти
+            </button>
+          </Link>
         </main>
       </div>
     </div>
